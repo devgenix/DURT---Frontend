4ea658af7a3915fb3780d497df2a898ecf3ed7df
Nworie Kingsley <obizzkingsley@gmail.com>
2022-07-08 01:07:08 +0100

    styling part1 complete

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            brand: '#512A23',
+            brand2: '#5A2F27',
+            black: '#000',
+            white: '#fff',
+            white2: '#CDC1BE',
+            white3: '#AC9793',
+            yellow: "#EDA911"
+          }
+      },
+    },
+    plugins: [],
+  }
